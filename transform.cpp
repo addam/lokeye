d1cbfb00efc565cf8d5c066c9d0bd476c394bf91
Adam Dominec <a.dominec@gmail.com>
2016-01-03 13:49:19 +0100

    transform: cleanup (rewritten using classes)

--- transform.cpp
+++ transform.cpp
@@ -1,168 +1,280 @@
 //#define WITH_OPENMP
+#include <vector>
+#include <functional>
+#include <tuple>
 #include "opencv2/imgproc.hpp"
 #include "opencv2/videoio.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/video.hpp"
 using cv::Point;
 using Vector = cv::Vec3f;
-using Mat = cv::Mat_<float>;
-using Image = cv::Mat_<Vector>;
+using Bitmap = cv::Mat_<Vector>;
+using Matrix = cv::Mat_<float>;
 
 const char *wint = "transform", *winr = "ref", *wind = "depth";
-
-Image ref;
-Image view;
-Mat cam, rot, trans;
-Mat depth;
 
 inline float pow2(float x)
 {
     return x*x;
 }
 
-Point convert(Vector v, const cv::Size &size)
+class Color : public cv::Vec3f
 {
-    float scale = std::min(size.width, size.height) / 2.f;
-    Point center{size.width/2, size.height/2};
-    return Point{int(v[0] * scale), int(v[1] * scale)} + center;
-}
+public:
+    Color(float r, float g, float b) : cv::Vec3f{r, g, b} {
+    }
+    Color(cv::Vec3f v) : cv::Vec3f{v} {
+    }
+    const static Color invalid;
+    static float difference(Color a, Color b) {
+        if (a == invalid or b == invalid) {
+            return 1;
+        }
+        return (std::abs(a[0] - b[0]) + std::abs(a[1] - b[1]) + std::abs(a[2] - b[2])) / 3.0;
+    }
+};
+const Color Color::invalid{-1, -1, -1};
 
-Vector convert(Point p, float z, const cv::Size &size)
-{
-    float scale = std::min(size.width, size.height) / 2.f;
-    Point center{size.width/2, size.height/2};
-    p -= center;
-    return Vector{p.x / scale, p.y / scale, z};
-}
+struct Iterrect : public cv::Rect {
+    Iterrect(const cv::Rect &rect) : cv::Rect{rect} {
+    }
+    struct Iterator : public Point {
+        int left, right;
+        Iterator(const Point &p, int right) : Point{p}, left{p.x}, right{right} {
+        }
+        Point& operator *() {
+            return *this;
+        }
+        bool operator != (const Iterator &other) const {
+            return y != other.y or x != other.x;
+        }
+        Iterator& operator++() {
+            if (++x >= right) {
+                x = left;
+                y ++;
+            }
+            return *this;
+        }
+    };
+    Iterator begin() const {
+        return Iterator{tl(), br().x};
+    }
+    Iterator end() const {
+        return Iterator{{tl().x, br().y}, br().x};
+    }
+};
 
-float difference(Vector a, Vector b)
+struct Image : public Bitmap
 {
-    return std::abs(a[0] - b[0]) + std::abs(a[1] - b[1]) + std::abs(a[2] - b[2]);
-}
+    cv::Rect area;
+    Image(int rows, int cols) : Bitmap{rows, cols}, area{0, 0, cols, rows} {
+    }
+    Image(const Bitmap &image) : Bitmap{image}, area{Point{0, 0}, image.size()} {
+    }
+    static Image read(const std::string &fileName) {
+        Bitmap bitmap;
+        cv::imread(fileName, cv::IMREAD_COLOR | cv::IMREAD_ANYDEPTH).convertTo(bitmap, CV_32FC3, 1./256);
+        return Image(bitmap);
+    }
+    Point convert(Vector v) const {
+        float scale = std::min(area.width, area.height) / 2.f;
+        Point center{area.width/2, area.height/2};
+        return Point{int(v[0] * scale), int(v[1] * scale)} + center;
+    }    
+    Color sample(Vector v) const {
+        Point p = convert(v);
+        return (area.contains(p)) ? (*this)(p) : (Color::invalid);
+    }
+    Image downscale() const {
+        Bitmap subimage;
+        cv::pyrDown(*this, subimage);
+        return Image{subimage};
+    }
+};
 
-Image transform()
+struct DepthImage : public Image
 {
-    static float best_total_error = 1e10;
-    float total_error = 0;
-    Mat projection = cam * trans * rot;
-    Image result = view.clone();
-    Mat zbuffer = Mat::ones(result.size()) * 1e8;
-    Mat errmap = Mat::zeros(result.size());
-    cv::Rect screen{Point{0, 0}, result.size()};
-    for (int i=0; i<ref.rows; i++) {
-        for (int j=0; j<ref.cols; j++) {
-            float z = depth(i, j);
-            if (z > 0) {
-                Vector v = convert(Point{j, i}, z, ref.size());
-                std::vector<Vector> tmp = {v};
-                cv::perspectiveTransform(tmp, tmp, projection);
-                Point p = convert(tmp[0], ref.size());
-                if (screen.contains(p)) {
-                    float d = difference(result(p), ref(i, j));
-                    errmap(i, j) = d / 3;
-                    total_error += d;
-                    if (zbuffer(p) > tmp[0][2]) {
-                        result(p) = ref(i, j);
-                        zbuffer(p) = tmp[0][2];
-                    }
-                } else {
-                    total_error += 3;
-                }
+    Matrix depth;
+    DepthImage(const Image &image) : Image{image} {
+        depth = Matrix::zeros(image.size());
+    }
+    DepthImage(const Image &image, const Matrix &depth) : Image{image}, depth{depth} {
+    }
+    Vector convert(Point p) const {
+        float z = depth(p);
+        float scale = std::min(area.width, area.height) / 2.f;
+        Point center{area.width/2, area.height/2};
+        p -= center;
+        return Vector{p.x / scale, p.y / scale, z};
+    }
+    DepthImage downscale() const {
+        Image subimage = ((Image)(*this)).downscale();
+        Matrix subdepth;
+        cv::pyrDown(depth, subdepth, subimage.size());
+        return DepthImage{subimage, subdepth};
+    }
+    float compare(Image view, Matrix projection) const {
+        const Image &self = *this;
+        float sumError = 0, sumWeight = 0;
+        std::vector<Vector> w{1};
+        Vector &v = w.front();
+        for (Point p : Iterrect{self.area}) {
+            v = convert(p);
+            cv::perspectiveTransform(w, w, projection);
+            if (v[2] > 0) {
+                Color sample = view.sample(v);
+                sumError += Color::difference(sample, self(p));
             }
         }
+        return sumError / float(self.size().area());
     }
-    cv::imshow(wind, errmap);
-    if (total_error > 0 and total_error < best_total_error) {
-        printf("error %f\n", total_error);
-        best_total_error = total_error;
+    float pyrCompare(Image view, Matrix projection, int sizeLimit=20) const {
+        const float bias = 1;
+        float score = 1 - compare(view, projection);
+        printf("compare %g : ", score);
+        DepthImage img = downscale();
+        do {
+            float here = 1 - img.compare(view, projection);
+            printf("%g : ", here);
+            score = (score + bias) * here;
+            img = img.downscale();
+            view = view.downscale();
+        } while (img.rows >= sizeLimit and img.cols >= sizeLimit);
+        return score;
     }
-    std::vector<Vector> axes{{0, 0, 0}, {0, 0, 1}, {0, 1, 0}, {1, 0, 0}};
-    cv::perspectiveTransform(axes, axes, projection);
-    Point origin{int(axes[0][0]), int(axes[0][1])};
-    for (int i=1; i<axes.size(); i++) {
-        cv::line(result, convert(axes[0], result.size()), convert(axes[i], result.size()), Vector{float(i==1), float(i==2), float(i==3)});
+};
+
+using ImagePair = std::pair<DepthImage, Image>;
+
+template<typename State>
+class Window
+{
+    using Callback = std::function<void(Window<State>&)>;
+    Callback callback;
+    static void onMouse(int event, int x, int y, int flags, void* ptr) {
+        Window &self = *((Window*)ptr);
+        self.prev = self.mouse;
+        self.mouse = {x, y};
+        if (event == cv::EVENT_LBUTTONDOWN) {
+            self.start = self.mouse;
+            self.pressed = true;
+        } else if (event == cv::EVENT_LBUTTONUP) {
+            self.pressed = false;
+        }
+        self.ctrl = flags & cv::EVENT_FLAG_CTRLKEY;
+        self.alt = flags & cv::EVENT_FLAG_ALTKEY;
+        self.shift = flags & cv::EVENT_FLAG_SHIFTKEY;
+        self.callback(self);
+    }
+    const std::string name;
+public:
+    ImagePair &images;
+    State state;
+    Point mouse, prev, start;
+    cv::Size size;
+    bool pressed{false};
+    bool ctrl{false}, alt{false}, shift{false};
+    Window(std::string name, ImagePair &images, Callback callback) : name{name}, images{images}, callback{callback} {
+        cv::namedWindow(name);
+        cv::setMouseCallback(name, onMouse, this);
+    }
+    void show(Bitmap image) {
+        size = image.size();
+        cv::imshow(name, image);
+    }
+    void show(Matrix image) {
+        size = image.size();
+        cv::imshow(name, image);
+    }
+};
+
+using ProjectionMatrix = std::array<Matrix, 3>;
+
+void viewRotate(Window<ProjectionMatrix> &window)
+{
+    assert (window.pressed);
+    cv::Point2f move = window.mouse - window.prev;
+    move *= 2.f / std::max(window.size.width, window.size.height);
+    if (window.shift) {
+        window.state[2](0, 3) -= move.x;
+        window.state[2](1, 3) -= move.y;
+    } else if (window.ctrl) {
+        window.state[2](2, 3) += move.y;
+        Matrix r = Matrix::eye(4, 4);
+        r(1, 0) = -(r(0, 1) = move.x);
+        r(0, 0) = r(1, 1) = std::sqrt(1 - pow2(move.y));
+        window.state[1] = r * window.state[1];
+    } else {
+        Matrix rx = Matrix::eye(4, 4), ry = Matrix::eye(4, 4);
+        rx(0, 2) = -(rx(2, 0) = move.x);
+        rx(0, 0) = rx(2, 2) = std::sqrt(1 - pow2(move.x));
+        ry(1, 2) = -(ry(2, 1) = move.y);
+        ry(1, 1) = ry(2, 2) = std::sqrt(1 - pow2(move.y));
+        window.state[1] = rx * ry * window.state[1];
+    }
+}
+
+Bitmap render(ImagePair images, Matrix projection)
+{
+    Bitmap result = images.second.clone();
+    Matrix zbuffer = 1e8 * Matrix::ones(images.second.size());
+    Matrix &depth = images.first.depth;
+    std::vector<Vector> w{1};
+    Vector &v = w.front();
+    for (Point p : Iterrect{images.first.area}) {
+        v = images.first.convert(p);
+        if (v[2] == 0) {
+            continue;
+        }
+        cv::perspectiveTransform(w, w, projection);
+        Point t = images.second.convert(v);
+        if (images.second.area.contains(t) and zbuffer(t) > v[2]) {
+            result(t) = images.first(p);
+            zbuffer(t) = v[2];
+        }
     }
     return result;
 }
 
-void redraw()
+void viewUpdate(Window<ProjectionMatrix> &window)
 {
-    Image img = transform();
-    imshow(wint, img);
+    static float bestScore = 0;
+    if (window.pressed) {
+        viewRotate(window);
+        Matrix projection = window.state[0] * window.state[2] * window.state[1];
+        window.show(render(window.images, projection));
+        float score = window.images.first.pyrCompare(window.images.second, projection);
+        if (score > bestScore) {
+            bestScore = score;
+            printf("score %g\n", score);
+        }
+    }
 }
 
-static void viewEvent(int event, int x, int y, int flags, void* ptr)
+void depthPaint(Window<int> &window)
 {
-    static bool pressed{false};
-    static int prev_x, prev_y;
-	if (event == cv::EVENT_LBUTTONUP) {
-        pressed = false;
-    } else if (event == cv::EVENT_LBUTTONDOWN) {
-        prev_x = x;
-        prev_y = y;
-        pressed = true;
-    }
-    if (pressed) {
-        float dx = 2.f * (x - prev_x) / (ref.cols);
-        float dy = 2.f * (y - prev_y) / (ref.rows);
-        if (flags & cv::EVENT_FLAG_SHIFTKEY) {
-            trans(0, 3) -= dx;
-            trans(1, 3) -= dy;
-        } else if (flags & cv::EVENT_FLAG_CTRLKEY) {
-            trans(2, 3) += dy;
-            Mat r = Mat::eye(4, 4);
-            r(1, 0) = -(r(0, 1) = dx);
-            r(0, 0) = r(1, 1) = std::sqrt(1 - pow2(dx));
-            rot = r * rot;
+    static bool wasPressed{false};
+    if (window.pressed or wasPressed) {
+        cv::Rect shape{2 * window.start - window.mouse, window.mouse};
+        shape &= cv::Rect{Point{0, 0}, window.size};
+        window.state = 0;
+        if (window.pressed) {
+            Bitmap tmp = window.images.first.clone();
+            cv::ellipse(tmp, cv::RotatedRect{window.start, shape.size(), 0.f}, 1.f);
+            window.show(tmp);
         } else {
-            Mat rx = Mat::eye(4, 4), ry = Mat::eye(4, 4);
-            rx(0, 2) = -(rx(2, 0) = dx);
-            rx(0, 0) = rx(2, 2) = std::sqrt(1 - pow2(dx));
-            ry(1, 2) = -(ry(2, 1) = dy);
-            ry(1, 1) = ry(2, 2) = std::sqrt(1 - pow2(dy));
-            rot = rx * ry * rot;
-        }
-        prev_x = x;
-        prev_y = y;
-        redraw();
-    }
-}
-
-static void drawEvent(int event, int x, int y, int flags, void* ptr)
-{
-    static bool pressed{false};
-    static float start_x, start_y;
-	if (event == cv::EVENT_LBUTTONDOWN) {
-        if (flags & cv::EVENT_FLAG_CTRLKEY) {
-            start_x = x;
-            start_y = y;
-            pressed = true;
-        }
-    } else if (event == cv::EVENT_LBUTTONUP) {
-        if (pressed) {
-            int sx = start_x, sy = start_y;
-            int width = std::abs(sx - x), height = std::abs(sy - y);
-            int left = std::max(0, sx - width), top = std::max(0, sy - height), right = std::min(depth.cols - 1, sx + width), bottom = std::min(depth.rows - 1, sy + height);
-            float scale = 2.f * std::min(width, height) / std::min(depth.rows, depth.cols);
-            for (int i=top; i < bottom; i++) {
-                for (int j=left; j < right; j++) {
-                    float radius = pow2(float(sx - j) / width) + pow2(float(sy - i) / height);
-                    if (radius < 1) {
-                        depth(i, j) += scale * std::sqrt(1 - radius);
-                    }
+            float scale = std::min(shape.width, shape.height) / float(std::min(window.size.height, window.size.width));
+            Matrix &depth = window.images.first.depth;
+            for (Point p : Iterrect{shape}) {
+                Point diff = p - window.start;
+                float radius = pow2(diff.x * 2.f / shape.width) + pow2(diff.y * 2.f / shape.height);
+                if (radius < 1) {
+                    depth(p) += scale * std::sqrt(1 - radius);
                 }
             }
-            cv::imshow(winr, ref);
-            cv::imshow(wind, depth);
-            pressed = false;
-        } else {
+            window.show(window.images.first);
         }
-    } else if (pressed) {
-        Image tmp = ref.clone();
-        cv::ellipse(tmp, cv::RotatedRect{cv::Point2f{start_x, start_y}, cv::Point2f{2.f*std::abs(float(x) - start_x), 2.f*std::abs(float(y) - start_y)}, 0.f}, cv::Scalar{0.f, 0.f, 1.f});
-        cv::imshow(winr, tmp);
-    } else if (flags & cv::EVENT_FLAG_ALTKEY) {
-    } else {
+        wasPressed = window.pressed;
     }
 }
 
@@ -172,23 +284,16 @@ int main(int argc, char** argv)
         fprintf(stderr, "usage: transform (reference.jpg) (view.jpg)\n");
         return 1;
     }
-    cam = Mat::eye(4, 4);
-    cam(3, 2) = 35.f / ((argc > 3) ? atof(argv[3]) : 30.f);
-    rot = -1 * Mat::eye(4, 4);
-    trans = Mat::eye(4, 4);
-    trans(2, 3) = -2;
-    cv::imread(argv[1], cv::IMREAD_COLOR | cv::IMREAD_ANYDEPTH).convertTo(ref, CV_32FC3, 1./256);
-    cv::imread(argv[2], cv::IMREAD_COLOR | cv::IMREAD_ANYDEPTH).convertTo(view, CV_32FC3, 1./256);
-    depth = Mat::zeros(ref.size());
-    
-    cv::namedWindow(winr, 1);
-    cv::setMouseCallback(winr, drawEvent, 0);
-    imshow(winr, ref);
-    cv::namedWindow(wint, 2);
-    cv::setMouseCallback(wint, viewEvent, 0);
-    redraw();
-    cv::namedWindow(wind, 3);
-    imshow(wind, depth);
-    while (char(cv::waitKey()) != 27);
+    ImagePair images{Image::read(argv[1]), Image::read(argv[2])};
+    Window<int> reference("reference", images, depthPaint);
+    reference.show(images.first);
+    Window<ProjectionMatrix> view("view", images, viewUpdate);
+    view.state[0] = Matrix::eye(4, 4);
+    view.state[0](3, 2) = 35.f / ((argc > 3) ? atof(argv[3]) : 30.f);
+    view.state[1] = -1 * Matrix::eye(4, 4);
+    view.state[2] = Matrix::eye(4, 4);
+    view.state[2](2, 3) = -2;
+    view.show(images.second);
+    do {} while (char(cv::waitKey()) != 27);
     return 0;
 }
