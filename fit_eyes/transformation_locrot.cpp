e0ed01508902db9c62700f8fec7bd30afcd13117
Adam Dominec <a.dominec@gmail.com>
2016-04-12 11:35:36 +0200

    fit_eyes: fix an error in derivatives

--- fit_eyes/transformation_locrot.cpp
+++ fit_eyes/transformation_locrot.cpp
@@ -15,9 +15,10 @@ Vector2 Transformation::operator () (Vector2 v) const
 
 Matrix23 Transformation::grad(Vector2 v) const
 {
-    float angle = params[2], s = sin(angle), c = cos(angle);
+    float coef = 3.f / static_params[2];
+    float angle = coef * params[2], s = sin(angle), c = cos(angle);
     v = Vector2(v[0] - static_params[0], v[1] - static_params[1]);
-    return Matrix23{1, 0, -v[0] * s - v[1] * c, 0, 1, v[0] * c - v[1] * s};
+    return Matrix23{1, 0, coef * (-v[0] * s - v[1] * c), 0, 1, coef * (v[0] * c - v[1] * s)};
 }
 
 Vector2 Transformation::inverse(Vector2 v) const
