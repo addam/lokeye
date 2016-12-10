6ead002535e52a6d9a531264aaec8bc10c32d7c8
Adam Dominec <a.dominec@gmail.com>
2016-12-11 00:28:45 +0100

    add test_transformation and transformation_doc.h

---
+++ fit_eyes/test_transformation.cpp
@@ -0,0 +1,112 @@
+#include "main.h"
+#include "optimization.h"
+#include <iostream>
+#include <random>
+
+using Params = Transformation::Params;
+const int param_cn = Params::rows * Params::cols;
+using Gradient = cv::Matx<float, 2, param_cn>;
+using Rng = std::uniform_real_distribution<>;
+
+template<int N, int M>
+void param_set(cv::Matx<float, N, M> &params, int i, float value)
+{
+    params(i/Params::cols, i%Params::cols) = value;
+}
+
+template<int N>
+void param_set(cv::Vec<float, N> &params, int i, float value)
+{
+    params(i) = value;
+}
+
+template<int N, int M>
+float param_get(const cv::Matx<float, N, M>  &params, int i)
+{
+    return params(i/Params::cols, i%Params::cols);
+}
+
+template<int N>
+float param_get(const  cv::Vec<float, N> &params, int i)
+{
+    return params(i);
+}
+
+std::basic_ostream<char> &operator<<(std::basic_ostream<char> &stream, const std::array<cv::Vec<float, 2>, 4> arr)
+{
+    using std::endl;
+    return (stream << arr[0] << endl << arr[1] << endl << arr[2] << endl << arr[3]);
+}
+    
+
+Params rand_params(std::minstd_rand &gen)
+{
+    Params result;
+    for (int i=0; i<param_cn; ++i) {
+        param_set(result, i, Rng(-1, 1)(gen));
+    }
+    return result;
+}
+
+cv::Point2f rand_point(std::minstd_rand &gen)
+{
+    Vector2 result;
+    result[0] = Rng(0, 640)(gen);
+    result[1] = Rng(0, 480)(gen);
+    return result;
+}
+
+Gradient analytic(const Transformation &tsf, Vector2 point)
+{
+    Gradient result;
+    for (int i=0; i<2; ++i) {
+        const Params row = tsf.d(point, i);
+        for (int j=0; j<param_cn; ++j) {
+            result(i, j) = param_get(row, j);
+        }
+    }
+    return result;
+}
+
+Gradient numeric(const Transformation &tsf, Vector2 point, float delta)
+{
+    Gradient result;
+    Vector2 tsf_point = tsf(point);
+    for (int j=0; j<param_cn; ++j) {
+        Params elem;
+        for (int t=0; t<param_cn; ++t) {
+            param_set(elem, j, (j == t) ? delta : 0);
+        }
+        Vector2 col = (tsf + elem)(point) - tsf_point;
+        for (int i=0; i<2; ++i) {
+            result(i, j) = col(i) / delta;
+        }
+    }
+    return result;
+}
+
+int main(int argc, char** argv)
+{
+    using std::cout;
+    using std::endl;
+    std::random_device rd;
+    std::minstd_rand gen(rd());
+    Region region(rand_point(gen), rand_point(gen));
+    cout << region << endl;
+    Transformation tsf(region);
+    Vector2 point = rand_point(gen);
+    Params r = rand_params(gen);
+    cout << "Static:" << endl << tsf.static_params << endl;
+    cout << tsf.params << endl << r << endl;
+    #if 0
+        tsf += r;
+        cout << tsf.params << endl;
+    #endif
+    cout << "Analytic:" << endl << analytic(tsf, point) << endl;
+    float delta = 1;
+    for (int i=0; i<6; ++i) {
+        cout << "Numeric, delta " << delta << ":" << endl << numeric(tsf, point, delta) << endl;
+        delta /= 10;
+    }
+    return 0;
+}
---
+++ fit_eyes/transformation_doc.h
@@ -0,0 +1,51 @@
+#error This file is only for documenation purposes.
+
+struct Transformation
+{
+    using Params = VectorN;
+    const StaticParamsType static_params;
+    InternalParamsType params;
+    
+    Transformation();
+    
+    /** Initialization with known area of interest
+     */
+    Transformation(Region region);
+    
+    /** Copy constructor
+     */
+    Transformation(decltype(params), decltype(static_params));
+    
+    /** Update params so as to mimic another transformation
+     */
+    Transformation& operator = (const Transformation&);
+    
+    /** Increase params by a differential amount
+     */
+    Transformation operator + (Params) const;
+    Transformation& operator += (Params);
+
+	/** Transform from reference to view space
+	 */
+    Vector2 operator () (Vector2) const;
+    Vector2 operator () (Pixel p) const { return (*this)(to_vector(p)); }
+	
+	/** Transform a bounding box from reference to view space
+	 */
+    Region operator () (Region) const;
+    
+    /** Estimate the scale factor
+     */
+    float scale(Vector2) const;
+    Vector2 operator - (const Transformation&) const;
+
+    /** Transformation from view to reference space
+     */
+    Transformation inverse() const;
+    Vector2 inverse(Vector2) const { return inverse()(v); }
+
+    /** Derivative of view space wrt. params
+     * @param direction 0 for x, 1 for y
+     */
+    Params d(Vector2, int direction) const;
+};
