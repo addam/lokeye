2a1d0b49ed2aeb015e585fd6920237e8732701b6
Adam Dominec <a.dominec@gmail.com>
2017-07-28 08:15:26 +0200

    update test_face

--- src/test_face.cpp
+++ src/test_face.cpp
@@ -27,18 +27,8 @@ void print(const Matrix33 &params)
     std::cout << "Main transformation: " << params << std::endl;
 }
 
-int main(int argc, char** argv)
+void track_interactive(VideoCapture &cam, bool is_interactive, bool is_verbose)
 {
-	bool is_interactive = false, is_verbose = false;
-	for (int i=1; i<argc; ++i) {
-		string arg(argv[i]);
-		if (arg == "-i") {
-			is_interactive = true;
-		} else if (arg == "-v") {
-			is_verbose = true;
-        }
-    }
-    VideoCapture cam{0};
     Bitmap3 image;
     for (int i=0; i<10; i++) {
         assert (image.read(cam));
@@ -64,15 +54,37 @@ int main(int argc, char** argv)
     int i;
     for (i=0; char(cv::waitKey(1)) != 27 and image.read(cam); ++i) {
         state.refit(image);
-        TimePoint time_now = std::chrono::high_resolution_clock::now();
-        float duration = std::chrono::duration_cast<std::chrono::duration<float>>(time_now - time_prev).count();
-        //std::cout << "Main transformation: " << state.main_tsf.params << ", face parameters: " << state() << ", " << 1 / duration << " fps" << std::endl;
-        print(state.main_tsf.params);
-        std::cout << "Face parameters: " << state() << ", " << 1 / duration << " fps" << std::endl;
-        time_prev = time_now;
+        if (is_verbose) {
+            TimePoint time_now = std::chrono::high_resolution_clock::now();
+            float duration = std::chrono::duration_cast<std::chrono::duration<float>>(time_now - time_prev).count();
+            //std::cout << "Main transformation: " << state.main_tsf.params << ", face parameters: " << state() << ", " << 1 / duration << " fps" << std::endl;
+            print(state.main_tsf.params);
+            std::cout << "Face parameters: " << state() << ", " << 1 / duration << " fps" << std::endl;
+            time_prev = time_now;
+        }
         state.render(image, "tracking");
     }
     float duration = std::chrono::duration_cast<std::chrono::duration<float>>(std::chrono::high_resolution_clock::now() - time_start).count();
     printf("%i frames in %g seconds ~= %g fps\n", i, duration, i / duration);
+}
+
+int main(int argc, char** argv)
+{
+	bool is_interactive = false, is_verbose = false;
+	for (int i=1; i<argc; ++i) {
+		string arg(argv[i]);
+		if (arg == "-i") {
+			is_interactive = true;
+		} else if (arg == "-v") {
+			is_verbose = true;
+        }
+    }
+    VideoCapture cam{0};
+    try {
+        track_interactive(cam, is_interactive, is_verbose);
+    } catch (NoFaceException) {
+        std::cerr << "No face initialized." << std::endl;
+        return 1;
+    }
     return 0;
 }
